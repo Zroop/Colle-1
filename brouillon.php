4934ae6e33936e4107d3df669eb0925992a73a12
Mohamed Nairi <mohamednairi4@gmail.com>
2016-07-29 15:37:59 +0200

    voila

---
+++ brouillon.php
@@ -0,0 +1,41 @@
+<?php
+function colle($x, $y)
+{
+  if ((!is_int($x)) OR (!is_int($y)))
+  {
+    return "Mauvais parametre";
+  }
+  if (($x < 0) OR ($y < 0))
+  {
+    return "Mauvais parametre";
+  }
+  for ($j=1; $j <= $y; $j++)
+  {//j = ligne, i = colonne
+    for ($i=1; $i <= $x; $i++)
+    {
+      if ($x==1 OR $y==1)
+      {
+        echo "B";
+      }
+      elseif (($j==1 && $i == 1) OR ($j == $y && $i == 1))
+      {
+        echo "A";
+      }
+      elseif (($j == 1 && $i == $x) OR ($j == $y && $i == $x))
+      {
+        echo "C";
+      }
+      elseif ($j==1 OR $j==$y OR $i==1 OR $i==$x)
+      {
+        echo "B";
+      }
+      else
+      {
+        echo " ";
+      }
+    }
+    echo "\n";
+  }
+}
+
+colle(0, 0);
